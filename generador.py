3c666dfa6a6ab24b58c519443e70864d4d85945a
camcinsocorp <35343110+camcinsocorp@users.noreply.github.com>
2018-08-24 23:58:11 -0500

    Carga 1

--- generador.py
+++ generador.py
@@ -1,35 +1,40 @@
 import sys
-def get_max_num(word):
-        maxi = 1
-        for x in range(len(word)):
-                maxi *= len(word)
+import csv
 
-        return maxi
+
+def get_max_num(word):
+    maxi = 1
+    for x in range(len(word)):
+        maxi *= len(word)
+
+    return maxi
+
 
 dic = sys.argv[1]
-print ("El numero de palabras que se generaran sera: "+str(get_max_num(dic))+"\nQuires proseguir?[Y/N]")
+print("El numero de palabras que se generaran sera: " +
+      str(get_max_num(dic))+"\nQuires proseguir?[Y/N]")
 resp = input(">")
 if resp != "y":
-        sys.exit(0)
+    sys.exit(0)
 
 lista = [0] * int(len(dic))
 palabra = ""
 a = 0
 while True:
-                palabra = ""
-                for w in range(len(lista)):
-                        if lista[w] == len(dic):
-                                lista[w] = 0
-                                try:
-                                        lista[w+1] += 1
-                                except:
-                                        pass
-                for x in lista:
-                        palabra += dic[x]
+    palabra = ""
+    for w in range(len(lista)):
+        if lista[w] == len(dic):
+            lista[w] = 0
+            try:
+                lista[w+1] += 1
+            except:
+                pass
+    for x in lista:
+        palabra += dic[x]
 
-                lista[0] += 1
+    lista[0] += 1
 
-                print (palabra)
-                a += 1
-                if a == get_max_num(dic):
-                        break
+    print(palabra)
+    a += 1
+    if a == get_max_num(dic):
+        break

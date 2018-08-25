02801a71d48e706fafccb2e9fed40e41b703c865
camcinsocorp <35343110+camcinsocorp@users.noreply.github.com>
2018-08-25 01:14:29 -0500

    Carga 4

--- generador.py
+++ generador.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import os
 
+
 def get_max_num(word):
     maxi = 1
     for x in range(len(word)):
@@ -11,12 +12,13 @@ def get_max_num(word):
 
 
 def create_file(name):
-    file = open("archivos/" + name +".txt", "a")
+    file = open("archivos/" + name + ".txt", "a")
     file.close()
 
+
 dic = sys.argv[1]
-file_name = input ("Ingrese el nombre del archivo a crear: ")
-create_file(file_name)
+file_name = input("Ingrese el nombre del archivo a crear: ")
+# create_file(file_name)
 
 
 print("El numero de palabras que se generaran sera: " +
@@ -32,6 +34,8 @@ palabra = ""
 
 alltext = ""
 a = 0
+
+
 while True:
     palabra = ""
     for w in range(len(lista)):
@@ -41,21 +45,24 @@ while True:
                 lista[w+1] += 1
             except:
                 pass
-    
 
     for x in lista:
-        palabra += dic[x] + ","
-    lista[0] += 1
-    print(palabra)
-    alltext += palabra
-    a += 1
+        palabra += dic[x]
+        lista[0] += 1
+        print(palabra)
+        alltext += palabra + ","
+        a += 1
     if a == resp2:
-        file = open("archivos/" + file_name + ".txt", "w")
+        print (alltext)
+        file = open("archivos/" + file_name + ".txt", "a")
         file.write(alltext)
         file.close()
         break
+
     if a == get_max_num(dic):
-        file = open("archivos/" + file_name + ".txt", "w")
+        print(alltext)
+
+        file = open("archivos/" + file_name + ".txt", "a")
         file.write(alltext)
         file.close()
         break
